308dc4988242523f80e4aa52845d56dcf26866ce
Caleb Spare <cespare@gmail.com>
2013-10-31 17:28:56 -0700

    [apachelog] Remove a bunch of tabs in the doc comment (again)

--- apachelog/apachelog.go
+++ apachelog/apachelog.go
@@ -6,26 +6,26 @@ Format strings:
     %%          A literal %.
     %B          Size of the full HTTP response in bytes, excluding headers.
     %b          Size of the full HTTP response in bytes, excluding headers. This
-		              is '-' rather than 0.
+                  is '-' rather than 0.
     %D          The time taken to serve the request, in microseconds. (Also see
-		              %T.)
+                  %T.)
     %h          The client's IP address. (This is a best guess only -- see
-		              hutil.RemoteIP.)
+                  hutil.RemoteIP.)
     %H          The request protocol.
     %{NAME}i    The contents of the request header called NAME.
     %m          The request method.
     %{NAME}o    The contents of the response header called NAME.
     %q          The query string (prepended with a ? if a query string exists;
-		              otherwise an empty string).
+                  otherwise an empty string).
     %r          First line of request (equivalent to '%m %U%q %H').
     %s          Response status code.
     %t          Time the request was received, formatted using ApacheTimeFormat
-		              and surrounded by '[]'.
+                  and surrounded by '[]'.
     %{FORMAT}t  Time the request was received, formatted using the supplied
-		              time.Format string FORMAT and surrounded by '[]'.
+                  time.Format string FORMAT and surrounded by '[]'.
     %T          The time taken to serve the request, in seconds. (Also see %D).
     %u          The remote user. May be bogus if the request was
-		              unauthenticated.
+                  unauthenticated.
     %U          The URL path requested, not including a query string.
 */
 package apachelog
